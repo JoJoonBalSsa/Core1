fbad4da97cbf695ccb7e5e00ef4c6369200b95a1
namaek2 <science4588@gmail.com>
2024-07-17 01:20:12 +0900

    function, variables mangled. now it works like private

--- main.py
+++ main.py
@@ -30,6 +30,6 @@ def main(java_folder_path, output_folder):
 
 if __name__ == "__main__":
     # Specify the folder containing Java files
-    #java_folder_path = '/home/namaek_2/java-christmas-6-scienceNH'
-    java_folder_path = 'C:/Users/조준형/Desktop/S개발자_프로젝트/Core1/christmas'  
+    java_folder_path = '/home/namaek_2/java-christmas-6-scienceNH'
+    #java_folder_path = 'C:/Users/조준형/Desktop/S개발자_프로젝트/Core1/christmas'  
     main(java_folder_path, java_folder_path)
