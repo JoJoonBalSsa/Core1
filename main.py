2f8891296ebb42cb041f64b324ca7ec7d4fe142d
HaEun <112637518+yellowgree@users.noreply.github.com>
2024-08-22 22:03:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from taintAnalysis import taintAnalysis
 from analysisResultManager import analysisResultManager
-from MethodPositionLocator import MethodPositionLocator
+
 
 
 def create_result(flows):
-    with open("result.txt", 'w', encoding='utf-8') as file:  # 결과 파일 생성
+    with open("result1.txt", 'w', encoding='utf-8') as file:  # 결과 파일 생성
         for (class_method, var), value in flows.items():
             file.write("Tainted Variable:\n")
             file.write(f"{class_method}, {var}\n")
@@ -35,33 +35,31 @@ def main(java_folder_path, output_folder):
 
 
 def __analyze_method(flows):
-    json_file_path = "C:/Users/gkds0/OneDrive/바탕 화면/core1/Core1/analysis_result1.json" 
+    json_file_path = "C:/Users/gkds0/OneDrive/바탕 화면/core1/Core1/analysis_result.json" 
     result = analysisResultManager(json_file_path)  # JSON 파일 경로를 지정
     tainted = taintAnalysis(java_folder_path)
-
 
     for flow in flows:
         sensitivity = flow[0]  # 민감도 값
 
-        method_full_path = flow[1]
-        # method_name을 추출
-        parts = method_full_path.split('.')
-        if len(parts) == 2 or 3:
-            method_name = parts[1]  # 두 번째 마지막 부분을 method_name으로 설정
+        for count in range(1, len(flow)):
+            method_full_path = flow[count]
+            # method_name을 추출
+            parts = method_full_path.split('.')
+            if len(parts) == 2 or len(parts) == 3:
+                method_name = parts[1]
 
 
-        current_path = tainted._get_file_path(method_name)  # current_path는 실제 코드에 맞게 설정
+            cut_tree = tainted._get_cut_tree(method_name)
 
-        source_code = tainted._extract_method_source_code(method_name)
-        
-        # 트리 위치 계산
-        tree_position = tainted._get_position
-        
-        cut_tree = tainted._get_cut_tree(method_name)
+            current_path = tainted._file_path  # current_path는 실제 코드에 맞게 설정
+            tree_position = tainted._get_position
 
-        # create_flow 함수가 반환하는 값을 flow_data 리스트에 추가
-        result.append(sensitivity, current_path, method_name, tree_position, cut_tree, source_code)
+            source_code = tainted._extract_method_source_code()
+            
 
+            result.append(sensitivity, current_path, method_name, tree_position, cut_tree, source_code)
+    
 
 
 if __name__ == "__main__":
