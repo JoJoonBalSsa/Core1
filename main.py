406dd9a3da2242def1545eb3a020127a750ad38f
Lanternbit <seolg0810@gmail.com>
2024-08-29 15:00:59 +0900

    Update JSON_FORMAT and CALL_taintAnalysis.py_ONCE

--- main.py
+++ main.py
@@ -31,27 +31,30 @@ def main(java_folder_path, output_folder):
 
     print_result(f_flow)
     create_result(tainted.flows)
-    __analyze_method(f_flow)
+    __analyze_method(f_flow, tainted)
 
 
-def __analyze_method(flows):
+def __analyze_method(flows, tainted):
     json_file_path = "./analysis_result.json" 
     result = analysisResultManager(json_file_path)
-    tainted = taintAnalysis(java_folder_path)
 
     for flow in flows:
         sensitivity = flow[0]  # 민감도 값
 
         for count in range(1, len(flow)):
             method_full_path = flow[count]
-            parts = method_full_path.split('.')
-            method_name = parts[1]
+            print(method_full_path)
+            big_parts = method_full_path.split(',') 
+            if len(big_parts) == 1:
+                big_parts.append("")
+            parts = big_parts[0].split('.')
+            little_method_name = parts[1]
 
-            cut_tree = tainted._get_cut_tree(method_name)
+            cut_tree = tainted._get_cut_tree(little_method_name)
             current_path = tainted._file_path
             tree_position = tainted._get_position
             source_code = tainted._extract_method_source_code()
-            method_name = method_full_path # 추가
+            method_name = method_full_path
 
             result.append(sensitivity, current_path, method_name, tree_position, cut_tree, source_code)
 
